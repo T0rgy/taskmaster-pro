d7747b7f53046c3286607581cbc571becbd0aff3
Torgy <jctorge@gmail.com>
2022-03-24 18:52:47 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,16 +54,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active")
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active")
   },
   update: function() {
     var tempArr = [];
@@ -96,6 +98,8 @@ $(".card .list-group").sortable({
   stop: function(event) {
     $(this).removeClass("dropover");
   }
+
+  
 });
 
 // drop into trash icon
@@ -104,13 +108,12 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
-
   },
   over: function(event, ui) {
-    console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function(event, ui) {
-    console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active")
   }
 });
 
@@ -149,7 +152,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
